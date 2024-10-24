5d99263f3bc2227e1cfb02a8501f5706e2e9fede
TelegramMiniApp <erkemyrzaa@gmail.com>
2024-10-25 00:14:05 +0500

    interface 0.2 bad

--- MiniApp/urls.py
+++ MiniApp/urls.py
@@ -22,24 +22,25 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 from django.shortcuts import render, get_object_or_404
 from django.conf import settings
+import json
 @csrf_exempt
 def profile_view(request):
     # Получение всех профилей
     profiles = Profile.objects.all()
-    return render(request, 'profile.html', {'profiles': profiles})
-@csrf_exempt
-def lessons_view(request):
-    # Получаем telegram_id из GET параметров
-    telegram_id = request.GET.get('telegram_id')
-
-    # Ищем пользователя по telegram_id
-    if telegram_id:
-        profile = get_object_or_404(Profile, telegram_id=telegram_id)
-        username = profile.username
-        coin = profile.coin
-        return render(request, 'lessons.html', {'username': username, 'coin': coin})
-    else:
-        return JsonResponse({'error': 'Telegram ID not provided'}, status=400)
+    return render(request, 'temp_files/profile.html', {'profiles': profiles})
+#@csrf_exempt
+# def lessons_view(request):
+#     # Получаем telegram_id из GET параметров
+#     telegram_id = request.GET.get('telegram_id')
+#
+#     # Ищем пользователя по telegram_id
+#     if telegram_id:
+#         profile = get_object_or_404(Profile, telegram_id=telegram_id)
+#         username = profile.username
+#         coin = profile.coin
+#         return render(request, 'temp_files/lessons.html', {'username': username, 'coin': coin})
+#     else:
+#         return JsonResponse({'error': 'Telegram ID not provided'}, status=400)
 @csrf_exempt
 def lesson_view(request):
     # Получаем telegram_id из GET параметров
@@ -73,35 +74,36 @@ def register_user(request):
         return JsonResponse({'status': 'error', 'message': 'Invalid data'}, status=400)
 
     return JsonResponse({'status': 'error', 'message': 'Invalid request method'}, status=405)
-@csrf_exempt
-def city_view(request):
-    telegram_id = request.GET.get('telegram_id')
-
-    # Ищем пользователя по telegram_id
-    if telegram_id:
-        profile = get_object_or_404(Profile, telegram_id=telegram_id)
-        username = profile.username
-        coin = profile.coin
-        return render(request, 'city.html', {'username': username, 'coin': coin})
-    else:
-        return JsonResponse({'error': 'Telegram ID not provided'}, status=400)
-@csrf_exempt
-def test_view(request):
-    telegram_id = request.GET.get('telegram_id')
-    if telegram_id:
-        profile = get_object_or_404(Profile, telegram_id=telegram_id)
-        username = profile.username
-        coin = profile.coin
-        return render(request, 'test.html', {'username': username, 'coin': coin})
-    else:
-        return JsonResponse({'error': 'Telegram ID not provided'}, status=400)
+# @csrf_exempt
+# def city_view(request):
+#     telegram_id = request.GET.get('telegram_id')
+#
+#     # Ищем пользователя по telegram_id
+#     if telegram_id:
+#         profile = get_object_or_404(Profile, telegram_id=telegram_id)
+#         username = profile.username
+#         coin = profile.coin
+#         return render(request, 'temp_files/city.html', {'username': username, 'coin': coin})
+#     else:
+#         return JsonResponse({'error': 'Telegram ID not provided'}, status=400)
+# @csrf_exempt
+# def test_view(request):
+#     telegram_id = request.GET.get('telegram_id')
+#     if telegram_id:
+#         profile = get_object_or_404(Profile, telegram_id=telegram_id)
+#         username = profile.username
+#         coin = profile.coin
+#         return render(request, 'temp_files/test.html', {'username': username, 'coin': coin})
+#     else:
+#         return JsonResponse({'error': 'Telegram ID not provided'}, status=400)
 
 
 def home_view(request):
     telegram_id = request.GET.get('telegram_id')
+    username = request.GET.get('username')
 
     # Ищем пользователя по telegram_id
-    if telegram_id:
+    if telegram_id or username:
         profile = get_object_or_404(Profile, telegram_id=telegram_id)
         username = profile.username
         coin = profile.coin
@@ -110,15 +112,67 @@ def home_view(request):
         return render(request, 'home.html', {'username': username, 'coin': coin})
     else:
         return render(request, 'error.html', {'error': 'Telegram ID not provided'})
+
+
+def test_view(request):
+    # Получаем username и coin из запроса (или профиля)
+    telegram_id = request.GET.get('telegram_id')
+    if telegram_id:
+        profile = get_object_or_404(Profile, telegram_id=telegram_id)
+        username = profile.username
+        coin = profile.coin
+    # Передаем данные в шаблон
+        return render(request, 'test.html', {'username': username, 'coin': coin, 'profile' : profile, 'telegram_id' : telegram_id})
+    else:
+        return render(request, 'error.html')
+
+@csrf_exempt
+def update_coins(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            telegram_id = data.get('telegram_id')
+            coins_to_add = data.get('coins')
+
+            if telegram_id and coins_to_add:
+                profile = Profile.objects.get(telegram_id=telegram_id)
+                profile.coin += coins_to_add  # Добавляем количество коинов
+                profile.save()
+
+                return JsonResponse({'status': 'success', 'new_balance': profile.coin})
+            else:
+                return JsonResponse({'status': 'error', 'message': 'Invalid data'})
+        except Profile.DoesNotExist:
+            return JsonResponse({'status': 'error', 'message': 'Profile not found'})
+    else:
+        return JsonResponse({'status': 'error', 'message': 'Invalid request method'})
+
+
+def city_view(request):
+    telegram_id = request.GET.get('telegram_id')
+
+    # Проверяем наличие telegram_id
+    if telegram_id:
+        profile = get_object_or_404(Profile, telegram_id=telegram_id)
+        username = profile.username
+        coin = profile.coin
+
+        # Рендерим шаблон city.html с передачей данных
+        return render(request, 'city.html', {'username': username, 'coin': coin, 'telegram_id': telegram_id})
+    else:
+        return render(request, 'error.html', {'error': 'Telegram ID not provided'})
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('profiles/', profile_view, name='profiles'),
-    path('lessons/', lessons_view, name='lessons'),
+   #path('profiles/', profile_view, name='profiles'),
+    #path('lessons/', lessons_view, name='lessons'),
     path('lesson/', lesson_view, name='lesson'),
     path('register_user/', register_user, name='register_user'),
-    path('city/', city_view, name='city'),
-    path('test/', test_view, name='test'),
+   # path('city/', city_view, name='city'),
+    #path('test/', test_view, name='test'),
     path('', home_view, name='home'),
+    path('test/', test_view, name='test'),
+    path('update_coins/', update_coins, name='update_coins'),
+    path('city/', city_view, name='city'),
 ]
 
 
